f6db032c28cd412cc32ea35a465e425f2a02aa16
Danil Grinin <danilgrinin@gmail.com>
2025-07-29 06:22:11 +0500

    new router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './oldrouter'
+import router from './router'
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
